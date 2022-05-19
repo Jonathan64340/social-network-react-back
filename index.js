bcb1b8734c93f3cb4e368455a5b654da4df4cda8
jonathan <you@example.comdomingues.jonathan95@gmail.com>
2022-05-20 01:04:29 +0200

    try

--- index.js
+++ index.js
@@ -10,22 +10,30 @@ const socketCorsUrl = require('./configs/socket.json');
 // Dependencies
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 
 // Cors
 const allowedOrigins = [process.env.allowedCorsOriginLocal, process.env.allowedCorsOrigin];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    console.log(origin)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     console.log(origin)
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 // Bodyparser
 app.use(express.json());

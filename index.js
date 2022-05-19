7c8419a2e59dea52f3c2007f62cf38ff5d68c4b4
jonathan <you@example.comdomingues.jonathan95@gmail.com>
2022-05-20 00:33:21 +0200

    use socket and express on same exposition

--- index.js
+++ index.js
@@ -45,9 +45,6 @@ CoreClass.init()
 
 const http = require('http').createServer(app);
 
-
-
-
 // Listen server
 const port = process.env.PORT || 5000;
 const io = require('socket.io')(app.listen(port, () => beautifierLogs('fgBlue', `This server is launched on port ${process.env.PORT || 5000}`)), {

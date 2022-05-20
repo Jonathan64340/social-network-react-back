c18f08d18f5084aa72f5ff75fc2491c17f4eb52d
jonathan <you@example.comdomingues.jonathan95@gmail.com>
2022-05-21 01:11:11 +0200

    hard fix messenger socket

--- controllers/messenger/messenger.js
+++ controllers/messenger/messenger.js
@@ -35,7 +35,7 @@ class Messenger extends Core {
         socket.on('messenger', (data) => {
             if (data.to) {
                 const to = data.to;
-                socket.to(to).emit('messenger', { ...data, from: data.from })
+                socket.to(to).emit('messenger', { ...data, from: socket.id })
             } else {
                 socket.emit('messenger', { ...data })
             }
